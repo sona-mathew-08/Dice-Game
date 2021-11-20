a244ed57720cecec374c1315162e88da2977a0da
Sona Mathew <sonamathewc@gmail.com>
2021-11-20 18:43:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1= Math.floor(Math.random()*6) +1;
+var randomDiceImage="dice" + randomNumber1 +".png";
+var randomImageSource="images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6) + 1;
+var randomImageSource2= "images/dice" + randomNumber2 +".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 WINS !!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent="Player 2 WINS !!";
+}
+else{
+    document.querySelector("h1").textContent="It's s DRAW";
+}
